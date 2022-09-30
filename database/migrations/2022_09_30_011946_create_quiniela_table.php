99cdcc2823aad09e143f1a17cd41f44f83e1fd3c
javierjolon <javierjolon.a@gmail.com>
2022-09-29 20:06:06 -0600

    se agregan las migraciones de la DB

--- database/migrations/2022_09_30_011946_create_quiniela_table.php
+++ database/migrations/2022_09_30_011946_create_quiniela_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateQuinielaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('quiniela', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer("userId");
+            $table->integer("gameId");
+            $table->integer("scoreTeam1");
+            $table->integer("scoreTeam2");
+            $table->integer("pointsXGame");
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('quiniela');
     }
 }
