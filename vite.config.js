7bad24fde283c0ab0ef1e3f83d8396fc61f44187
javierjolon <63754080+javierjolon@users.noreply.github.com>
2026-02-09 12:14:13 -0600

    configuraciones

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    server: {
+        host: 'kaleaquinielas.test', // 🔴 CLAVE
+        port: 5173,
+        strictPort: true,
+        cors: true,
+        hmr: {
+            host: 'kaleaquinielas.test',
+        },
+    },
+    plugins: [
+        laravel({
+            input: ['resources/js/app.jsx'],
+            refresh: true,
+        }),
+        react(),
+    ],
+});
